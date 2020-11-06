9cdb379490f0e076b3cb98d7b4a33ac29ad1a60f
DesiBerardo <db32@njit.edu>
2020-11-05 21:28:28 -0500

    Completed HW10 P3

--- HW10/HW10 P3.py
+++ HW10/HW10 P3.py
@@ -4,22 +4,16 @@ CS100 113
 11/5/2020
 '''
 
-def initialLetterCount(wordList):
+def ShareOneLetter(wordList):
     dict = {}
     for word in wordList:
-        if word not in dict:
-            dict[word] = [word]
-    for word in wordList:
-        for letter in word:
-            if letter in dict[word]:
-                print('test')
-                dict[word].append(word)
-
-
+        dict[word] = [word]
+        for word2 in wordList:
+            for letter in word2:
+                if letter in word2 and word2 not in dict[word]:
+                    dict[word].append(word2)
     return dict
-
-
 
 horton = ['I', 'say', 'what', 'I', 'mean', 'and', 'I', 'mean', 'what', 'I', 'say']
 
-print(initialLetterCount(horton))
+print(ShareOneLetter(horton))

52f1f7485cb5ec5c391d470438fefed2f1b1098d
DesiBerardo <db32@njit.edu>
2020-11-17 12:06:40 -0500

    added dog.py

--- class follow along.py
+++ class follow along.py
@@ -1,7 +1,19 @@
-inF = open('myfile.txt', 'r')
+def countVowels(text):
+    wordList = text.split()
+    vowels = 'aeiou'
+    d = {}
+    for word in wordList:
+        if word not in d:
+            d[word] = 0
+        for letter in word:
+            if letter in vowels:
+                d[word] += 1
+    return d
 
-text = inF.read()
 
-print(text)
 
-inF.close()
+
+
+
+abe = "government of the people by the people for the people shall not perish"
+print(countVowels(abe))
